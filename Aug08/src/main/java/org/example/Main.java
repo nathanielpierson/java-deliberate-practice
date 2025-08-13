fe6adb59b12c35983c78c10fa561c249abf013b6
nathanielpierson <nathanielepierson@gmail.com>
2025-08-12 20:41:00 -0400

    add Aug08 folder

---
+++ Aug08/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Write a program that uses a variable to store a number, then prints that
+        // number plus 10.
+        int number = 5;
+        System.out.println(number + 10);
+        // Write a program that uses a variable to store a word, then prints that word
+        // with all capital letters.
+        String word = "baziNga";
+        System.out.println(word.toUpperCase());
+        // Write a program that uses variables to store two numbers, then prints the
+        // numbers added together.
+        int first_number = 3;
+        int second_number = 24;
+        System.out.println(first_number + second_number);
+    }
+}
